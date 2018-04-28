6bbece608e3633137abd2fdd917d47c4a98644e2
Konrad Eichstaedt <konrad.eichstaedt@gmx.de>
2018-04-28 18:06:38 +0200

    Add First Registrierungs Seite to project

---
+++ src/main/java/de/eichstaedt/haushaltsbuch/application/Registrierung.java
@@ -0,0 +1,111 @@
+package de.eichstaedt.haushaltsbuch.application;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by konrad.eichstaedt@gmx.de on 28.04.18.
+ */
+
+@Component
+public class Registrierung {
+
+  private String vorname;
+
+  private String nachname;
+
+  private String email;
+
+  private String benutzername;
+
+  private String passwort;
+
+  private String passwortWiederholung;
+
+  private String strasse;
+
+  private String stadt;
+
+  private String postleitzahl;
+
+  private String land;
+
+  public String getVorname() {
+    return vorname;
+  }
+
+  public void setVorname(String vorname) {
+    this.vorname = vorname;
+  }
+
+  public String getNachname() {
+    return nachname;
+  }
+
+  public void setNachname(String nachname) {
+    this.nachname = nachname;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getBenutzername() {
+    return benutzername;
+  }
+
+  public void setBenutzername(String benutzername) {
+    this.benutzername = benutzername;
+  }
+
+  public String getPasswort() {
+    return passwort;
+  }
+
+  public void setPasswort(String passwort) {
+    this.passwort = passwort;
+  }
+
+  public String getPasswortWiederholung() {
+    return passwortWiederholung;
+  }
+
+  public void setPasswortWiederholung(String passwortWiederholung) {
+    this.passwortWiederholung = passwortWiederholung;
+  }
+
+  public String getStrasse() {
+    return strasse;
+  }
+
+  public void setStrasse(String strasse) {
+    this.strasse = strasse;
+  }
+
+  public String getStadt() {
+    return stadt;
+  }
+
+  public void setStadt(String stadt) {
+    this.stadt = stadt;
+  }
+
+  public String getPostleitzahl() {
+    return postleitzahl;
+  }
+
+  public void setPostleitzahl(String postleitzahl) {
+    this.postleitzahl = postleitzahl;
+  }
+
+  public String getLand() {
+    return land;
+  }
+
+  public void setLand(String land) {
+    this.land = land;
+  }
+}
---
+++ src/main/java/de/eichstaedt/haushaltsbuch/application/RegistrierungController.java
@@ -0,0 +1,35 @@
+package de.eichstaedt.haushaltsbuch.application;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by konrad.eichstaedt@gmx.de on 28.04.18.
+ */
+
+@Controller
+public class RegistrierungController {
+
+  @Autowired
+  private Registrierung registrierung;
+
+
+  private static final Logger logger = LoggerFactory.getLogger(RegistrierungController.class);
+
+  @RequestMapping(value = "registrierung")
+  public String registration(Model model) {
+
+    logger.info("GET Request for registration page ");
+
+    model.addAttribute("registration", registrierung);
+
+    return "registrierung";
+  }
+
+}
