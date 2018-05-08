9c9bd252d2e3be48b936ce47fa80fbbce8a07c38
konrad eichstaedt <konrad.eichstaedt@gmx.de>
2018-05-08 07:06:14 +0200

    Add AuthenticationPRovider

---
+++ src/main/java/de/eichstaedt/haushaltsbuch/infrastructure/HaushaltsbuchAuthenticationProvider.java
@@ -0,0 +1,48 @@
+package de.eichstaedt.haushaltsbuch.infrastructure;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Collection;
+
+public class HaushaltsbuchAuthenticationProvider implements AuthenticationProvider {
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        String username = authentication.getName();
+        String password = (String) authentication.getCredentials();
+
+        UserDetails user = userDetailsService.loadUserByUsername(username);
+
+        if (user == null || !user.getUsername().equals(username)) {
+            throw new BadCredentialsException("Benutzername oder Passwort unbekannt.");
+        }
+
+        if (!passwordEncoder.matches(password, user.getPassword())) {
+            throw new BadCredentialsException("Benutzername oder Passwort unbekannt.");
+        }
+
+        Collection<? extends GrantedAuthority> authorities = user.getAuthorities();
+
+        return new UsernamePasswordAuthenticationToken(user, password, authorities);
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return false;
+    }
+}
