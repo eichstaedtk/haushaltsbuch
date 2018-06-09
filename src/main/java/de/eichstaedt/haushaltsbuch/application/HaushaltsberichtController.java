8cdf952e88198e72d73d241b4383b046e60a3df6
Konrad Eichstaedt <konrad.eichstaedt@gmx.de>
2018-06-09 15:23:18 +0200

    Add first version for JPlot Chart Haushaltsbericht

---
+++ src/main/java/de/eichstaedt/haushaltsbuch/application/HaushaltsberichtController.java
@@ -0,0 +1,34 @@
+package de.eichstaedt.haushaltsbuch.application;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * Created by konrad.eichstaedt@gmx.de on 09.06.18.
+ */
+
+@Controller
+public class HaushaltsberichtController {
+
+  private static final Logger logger = LoggerFactory.getLogger(HaushaltsberichtController.class);
+
+  @GetMapping("/haushaltsbericht")
+  public ModelAndView oeffnen(ModelMap model,@AuthenticationPrincipal User accountDetails, @RequestParam(value = "buchid", required = false) String buchid)
+  {
+
+    logger.info("Request GET for haushaltsbericht");
+
+
+    model.addAttribute("monatszahlen",new double[]{23,56,100,45.60,100,450});
+
+    return new ModelAndView("haushaltsbericht",model);
+  }
+
+}
