55a9d428646eee59741917031a8c2d1693c53189
Konrad Eichstaedt <konrad.eichstaedt@gmx.de>
2018-07-15 10:10:18 +0200

    Add Test KategorieRepositoryTest

---
+++ src/test/java/de/eichstaedt/haushaltsbuch/domain/repository/KategorieRepositoryTest.java
@@ -0,0 +1,46 @@
+package de.eichstaedt.haushaltsbuch.domain.repository;
+
+import static org.hamcrest.Matchers.is;
+
+import de.eichstaedt.haushaltsbuch.domain.valueobjects.Kategorie;
+import java.util.Optional;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * Created by konrad.eichstaedt@gmx.de on 15.07.18.
+ */
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KategorieRepositoryTest {
+
+  @Autowired
+  private KategorieRepository kategorieRepository;
+
+  @Test
+  public void testSave() {
+
+    Kategorie kategorie = new Kategorie("Versicherung");
+
+    Kategorie saved = kategorieRepository.save(kategorie);
+
+    Assert.assertThat(kategorieRepository.findById("Versicherung").get(),is(kategorie));
+  }
+
+  @Test
+  public void testDelete() {
+
+    Kategorie kategorie = new Kategorie("Versicherung");
+
+    kategorieRepository.save(kategorie);
+
+    kategorieRepository.deleteById("Versicherung");
+
+    Assert.assertThat(kategorieRepository.findById("Versicherung"),is(Optional.empty()));
+  }
+}
