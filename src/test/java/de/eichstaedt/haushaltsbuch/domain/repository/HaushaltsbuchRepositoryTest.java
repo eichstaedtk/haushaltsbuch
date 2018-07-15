4c37bf6bef75c6c024e362b92882e217bd5eed6e
Konrad Eichstaedt <konrad.eichstaedt@gmx.de>
2018-07-15 10:05:20 +0200

    Add Test HaushaltsbuchRepositoryTest

---
+++ src/test/java/de/eichstaedt/haushaltsbuch/domain/repository/HaushaltsbuchRepositoryTest.java
@@ -0,0 +1,85 @@
+package de.eichstaedt.haushaltsbuch.domain.repository;
+
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.Matchers.nullValue;
+
+import de.eichstaedt.haushaltsbuch.domain.entities.Benutzer;
+import de.eichstaedt.haushaltsbuch.domain.entities.Haushaltsbuch;
+import java.util.Optional;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * Created by konrad.eichstaedt@gmx.de on 15.07.18.
+ */
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HaushaltsbuchRepositoryTest {
+
+  @Autowired
+  private HaushaltsbuchRepository haushaltsbuchRepository;
+
+  @Test
+  public void testSave() {
+
+    PasswordEncoder passwordEncoder = Mockito.mock(PasswordEncoder.class);
+
+    Mockito.when(passwordEncoder.encode("Start123")).thenReturn("3432423");
+
+    Benutzer benutzer = new Benutzer.BenutzerBuilder("konrad","konrad.eichstaedt@gmx.de","Start123",passwordEncoder).build();
+
+    Haushaltsbuch haushaltsbuch = new Haushaltsbuch("Buch 2018",benutzer);
+
+    Assert.assertThat(haushaltsbuch.getId(),is(nullValue()));
+
+    Haushaltsbuch saved = haushaltsbuchRepository.save(haushaltsbuch);
+
+    Assert.assertThat(saved.getId(),is(notNullValue()));
+
+    Assert.assertThat(saved.getName(),is("Buch 2018"));
+  }
+
+  @Test
+  public void testDelete() {
+
+    PasswordEncoder passwordEncoder = Mockito.mock(PasswordEncoder.class);
+
+    Mockito.when(passwordEncoder.encode("Start123")).thenReturn("3432423");
+
+    Benutzer benutzer = new Benutzer.BenutzerBuilder("konrad","konrad.eichstaedt@gmx.de","Start123",passwordEncoder).build();
+
+    Haushaltsbuch haushaltsbuch = new Haushaltsbuch("Buch 2018",benutzer);
+
+    Haushaltsbuch saved = haushaltsbuchRepository.save(haushaltsbuch);
+
+    haushaltsbuchRepository.delete(saved);
+
+    Assert.assertThat(haushaltsbuchRepository.findById(saved.getId()),is(Optional.empty()));
+  }
+
+  @Test
+  public void testfindByBesitzerBenutzername() {
+
+    PasswordEncoder passwordEncoder = Mockito.mock(PasswordEncoder.class);
+
+    Mockito.when(passwordEncoder.encode("Start123")).thenReturn("3432423");
+
+    Benutzer benutzer = new Benutzer.BenutzerBuilder("konrad","konrad.eichstaedt@gmx.de","Start123",passwordEncoder).build();
+
+    Haushaltsbuch haushaltsbuch = new Haushaltsbuch("Buch 2018",benutzer);
+
+    Haushaltsbuch saved = haushaltsbuchRepository.save(haushaltsbuch);
+
+    Assert.assertThat(haushaltsbuchRepository.findByBesitzerBenutzername("konrad").get(0),is(saved));
+
+
+  }
+}
