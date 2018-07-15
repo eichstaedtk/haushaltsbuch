2de52610b5c8a763a0c3a8855451523463c33ef1
Konrad Eichstaedt <konrad.eichstaedt@gmx.de>
2018-07-15 09:51:50 +0200

    Ad test ZahlungsflussTest

---
+++ src/test/java/de/eichstaedt/haushaltsbuch/domain/entities/ZahlungsflussTest.java
@@ -0,0 +1,35 @@
+package de.eichstaedt.haushaltsbuch.domain.entities;
+
+import static org.hamcrest.Matchers.is;
+
+import de.eichstaedt.haushaltsbuch.domain.valueobjects.Kategorie;
+import de.eichstaedt.haushaltsbuch.domain.valueobjects.Zahlungsintervall;
+import de.eichstaedt.haushaltsbuch.domain.valueobjects.Zahlungstyp;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by konrad.eichstaedt@gmx.de on 15.07.18.
+ */
+public class ZahlungsflussTest {
+
+  @Test
+  public void testCreation() {
+
+    Zahlungsfluss zahlungsfluss = new Zahlungsfluss("Beschreibung",new BigDecimal(2.45),new Kategorie("Test"),LocalDate.now(),Zahlungstyp.AUSGABE,Zahlungsintervall.EINMALIG,1l);
+
+    Assert.assertThat(zahlungsfluss.getTyp(),is(Zahlungstyp.AUSGABE));
+
+    Assert.assertThat(zahlungsfluss.getBeschreibung(),is("Beschreibung"));
+
+    Assert.assertThat(zahlungsfluss.getBetrag(),is(new BigDecimal(2.45)));
+
+    Assert.assertThat(zahlungsfluss.getBuchungsTag(),is(LocalDate.now()));
+
+    Assert.assertThat(zahlungsfluss.getZahlungsintervall(),is(Zahlungsintervall.EINMALIG));
+
+    Assert.assertThat(zahlungsfluss.getBuchid(),is(1l));
+  }
+}
